fc22016a914c88c79935f520cb0ff24f4d6a2888
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2017-06-29 14:35:54 -0500

    Se inicia proyecto

---
+++ server.js
@@ -0,0 +1,31 @@
+// Cargando ExpressJs
+const express = require('express');
+// Cargano path
+const path = require('path');
+// Cargando configuracion
+const config = require('./server/configure');
+// Creando una instancia de la aplicacion
+const app = express();
+// Estableciendo variables de entorno de la app
+app.set('port', process.env.PORT || 3000);
+app.set('ip', process.env.IP || "0.0.0.0");
+// Configurando la ruta de las vistas
+app.set('views', path.join(__dirname + '/views'));
+// Aplicando Configuraciones
+config(app);
+// Creando una ruta de prueba
+app.get('/', (req, res)=>{
+    res.send("Hola ITGAM");
+});
+// Consultando variable de entorno
+var IP = app.get('ip');
+var PORT = app.get('port');
+// Iniciando el servidor
+app.listen(PORT, IP, (err)=>{
+    if(err){
+        console.log(`> error: ${err}`);
+        throw err;
+    }
+    console.log(`> Servidor escuchando en http://${IP}:${PORT}`);
+});
+
---
+++ server/configure.js
@@ -0,0 +1,32 @@
+// Cargando dependencias
+var path = require('path'),
+    exphdn = require('express-handlebars'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    moment = require('moment'),
+    multer = require('multer');
+ var routes = require('./routes');
+
+module.exports = function(app){
+    // Agregando Middlewares Generales
+    app.use(morgan('dev'));
+    app.use(bodyParser.urlencoded({'extended':true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+    app.use(cookieParser('algun-valor-secreto-aqui'));
+    
+    // Configuracion de archivos estaticos
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
+    // Si la aplicacion esta en modo
+    // de desarrollo se usa errorhandler
+    if('development' === app.get('env')){
+        app.use(errorHandler());
+    }
+
+    return app;
+};
