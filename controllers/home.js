bdfe71fac2867fd5ebe7febeb3ae6c1569120e33
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2017-10-16 09:11:32 -0500

    Se crea el vm index para home controller

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,48 @@
 module.exports = {
     // Action Method
     index:(req, res)=>{
-        res.render("index");
+        // Creando el Viewmodel
+        var viewModel = {
+            images: [
+                {
+                    uniqueId: 1,
+                    title: "Sample test Image 1",
+                    description:'An awesome Image 1',
+                    filename: 'sample.png',
+                    views:0,
+                    likes:0,
+                    timestamp: Date.now()
+                },
+                {
+                    uniqueId: 2,
+                    title: "Sample test Image 2",
+                    description:'An awesome Image 2',
+                    filename: 'sample.png',
+                    views:0,
+                    likes:0,
+                    timestamp: Date.now()
+                },
+                {
+                    uniqueId: 3,
+                    title: "Sample test Image 3",
+                    description:'An awesome Image 3',
+                    filename: 'sample.png',
+                    views:0,
+                    likes:0,
+                    timestamp: Date.now()
+                },
+                {
+                    uniqueId: 4,
+                    title: "Sample test Image 4",
+                    description:'An awesome Image 4',
+                    filename: 'sample.png',
+                    views:0,
+                    likes:0,
+                    timestamp: Date.now()
+                }
+            ]
+        };
+        res.render("index", viewModel);
     }
 };
-
-// Todo Rendering views U2_7 primer slide
+// TO DO: U2_8#6 continuar
