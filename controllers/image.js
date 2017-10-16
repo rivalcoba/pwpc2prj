cdd5f08841fcfd90e5794da8178538f56ea3a3a0
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2017-10-16 11:13:27 -0500

    Se crea VM para image.index

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,37 @@
 module.exports = {
     // Action Methods
     index : (req, res)=>{
-        res.render("image");
+        // Agregando el ViewModel
+        var ViewModel = {
+            image: {
+                uniqueId: 1,
+                title: "Sample Image 1",
+                description: "Awesome Image",
+                filename: "sample.png",
+                views: 0,
+                likes: 0,
+                timestamp: Date.now() 
+            },
+            comments: [
+                {
+                    image_id: 1,
+                    email: "test@testing.com",
+                    name: "Test Tester",
+                    gravatar: "http://lorempixel.com/75/75/animals/1",
+                    comment: "This is my awesome opinion",
+                    timestamp: Date.now()
+                },
+                {
+                    image_id: 2,
+                    email: "test2@testing.com",
+                    name: "Test2 Tester2",
+                    gravatar: "http://lorempixel.com/75/75/animals/2",
+                    comment: "This is my awesome opinion 2",
+                    timestamp: Date.now()
+                }
+            ]
+        };
+        res.render("image", ViewModel);
     },
     create : (req, res)=>{
         res.end(`> Se crea nueva imagen`);
